598ef32808a3ac9ded552cba3725112d8274665d
Maria Eduarda Marques <eduardamarquesnoleto@gmail.com>
2025-04-23 13:33:19 -0300

    chore: configure ESLint and Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+
+module.exports = {
+    semi: true, // adiciona ponto e vírgula no final das linhas
+    trailingComma: 'all', // vírgula no final de objetos, arrays, etc.
+    singleQuote: true, // usa aspas simples em vez de aspas duplas
+    printWidth: 120, // quebra a linha se passar de 120 caracteres
+    tabWidth: 2, // define 2 espaços por tabulação
+};
